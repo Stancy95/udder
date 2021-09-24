a3c8bab26aedc33f71a85500c4525ad2507fe5e5
Stancy <stancy@zionstar.in>
2021-09-24 13:34:48 +0530

    Initial Commit

---
+++ udder/udderApp.swift
@@ -0,0 +1,17 @@
+//
+//  udderApp.swift
+//  udder
+//
+//  Created by samuel santhosh on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct udderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
